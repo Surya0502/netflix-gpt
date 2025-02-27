2fea2f3159f75d906b687ee17b001898bcebef5c
Suryakant Sharma <sharmasuryakant.jsr@gmail.com>
2025-02-28 00:44:44 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl font-bold underline">Hi</p>
+    </div>
+  );
+}
+
+export default App;
